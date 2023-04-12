41e2295af073fdcd86f4550db07f41833fd7e79b
amirb <amir.bullock9@gmail.com>
2023-04-12 10:51:27 -0400

    Add files to git

---
+++ data/student-data.js
@@ -0,0 +1,10 @@
+const students = [
+  {name: 'Amir B.', sei: true, _id: 153627},
+  {name: 'Bryan C.', sei: false, _id: 20094},
+  {name: 'Matt A.', sei: true, _id: 153628},
+  {name: 'Brendan B.', sei: true, _id: 153629}
+]
+
+export {
+  students
+}
---
+++ server.js
@@ -0,0 +1,41 @@
+// import modules
+
+import express from 'express'
+import { students } from './data/student-data.js'
+// create Express app
+
+const app = express()
+
+// configure the app (app.set)
+
+app.set('view engine', 'ejs')
+
+
+// mount Middleware (app.use)
+
+
+
+// mount routes
+
+app.get('/', function(req, res) {
+  res.send('<h1>sup dawg</h1>')
+})
+app.get('/home', function(req, res) {
+  res.render('home')
+})
+app.get('/students', function(req, res) {
+  res.send('students/index', {
+    //students (what we are defining) : students (the actual data)
+    students
+  })
+})
+
+app.get('/home/students/Amir', function(req, res) {
+  res.send('<h1>Amir B.</h1>')
+})
+
+//tell the app to listen on port 3000
+
+app.listen(3000, function() {
+  console.log('Listening on port 3000')
+})
